399e401bfc7aa1515573d781e7c80fb5675b3773
pbhandar <pbhandar@localhost>
2023-06-16 20:33:13 -0500

    add scripts to setup machines

--- scripts/cloudlab/setup-cachelib.sh
+++ scripts/cloudlab/setup-cachelib.sh
@@ -5,5 +5,8 @@ if [ ! -d "${HOME}/disk/CacheLib" ]; then
     git clone https://github.com/pbhandar2/CacheLib
 fi
 cd CacheLib 
+if [ ! -d "${HOME}/disk/CacheLib/phdthesis" ]; then
+    git clone https://github.com/pbhandar2/phdthesis
+fi
 git checkout active
 ./contrib/build.sh -j -d 
