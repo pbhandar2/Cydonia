0d9b2a8df7de2debb12dfb756cbc894ceeae3f50
Pranav Bhandari <pranav@mjolnir.mathcs.emory.edu>
2023-06-18 08:33:01 -0400

    update runner scirpt for MT cache replay

--- scripts/runner/runner.py
+++ scripts/runner/runner.py
@@ -4,12 +4,22 @@ import pathlib
 from cydonia.cachelib.ReplayConfig import ReplayConfig
 from cydonia.cachelib.Runner import Runner 
 
+DEFAULT_BACKING_FILE_PATH = pathlib.Path.home().joinpath("disk/disk.file")
+DEFAULT_NVM_FILE_PATH = pathlib.Path.home().joinpath("nvm/disk.file")
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run a cachebench block trace replay")
     parser.add_argument("trace_path", type=pathlib.Path, help="Path to block trace file")
     parser.add_argument("t1_size_mb", type=int, help="Size of tier-1 cache in MB")
     parser.add_argument("t2_size_mb", type=int, help="Size of tier-2 cache in MB")
-    parser.add_argument("backing_file_path", type=pathlib.Path, help="Path to file on backing storage")
+    parser.add_argument("--backing_file_path", 
+                            default=DEFAULT_BACKING_FILE_PATH, 
+                            type=pathlib.Path, 
+                            help="Path to file on backing storage")
+    parser.add_argument("--nvm_file_path", 
+                            default=DEFAULT_NVM_FILE_PATH,
+                            type=pathlib.Path, 
+                            help="Path to file on NVM device")
     args = parser.parse_args()
 
     config_path = pathlib.Path("/dev/shm/config.json")
@@ -17,8 +27,17 @@ if __name__ == "__main__":
     experiment_output_dir= pathlib.Path("/dev/shm/out.dump")
     usage_output_dir = pathlib.Path("/dev/shm/usage.csv")
 
-    config = ReplayConfig([str(args.trace_path.resolve())], [str(args.backing_file_path.resolve())], args.t1_size_mb)
+    kwargs = {}
+    if args.t2_size_mb > 0:
+        kwargs["nvmCacheSizeMB"] = args.t2_size_mb
+        kwargs["nvmCachePaths"] = [str(args.nvm_file_path.absolute())]
+
+    config = ReplayConfig([str(args.trace_path.resolve())], [str(args.backing_file_path.resolve())], args.t1_size_mb, **kwargs)
     config.generate_config_file(config_path)
 
-    runner = Runner()
-    runner.run(cachebench_binary_path, config_path, experiment_output_dir, usage_output_dir)
+
+
+    print(config)
+
+    #runner = Runner()
+    #runner.run(cachebench_binary_path, config_path, experiment_output_dir, usage_output_dir)
