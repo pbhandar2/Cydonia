57f6af9460309c23b7495af3a3854a0d8a86a7a1
Pranav Bhandari <pranav@mjolnir.mathcs.emory.edu>
2023-06-26 03:10:07 -0400

    fix the done key bug in run experiemtn

--- scripts/fast24/RunExperiment.py
+++ scripts/fast24/RunExperiment.py
@@ -106,17 +106,13 @@ class RunExperiment:
 
                 print("Completed-> Experiment {},{}", config.get_config(), cur_iteration)
                 # upload all necessary files 
-                done_s3_key_prefix = self.get_s3_key("live", workload, config.get_config(), cur_iteration)
+                done_s3_key_prefix = self.get_s3_key("done", workload, config.get_config(), cur_iteration)
                 
                 self.s3.upload_s3_obj("{}/{}".format(done_s3_key_prefix, self.config_file_path.name), str(self.config_file_path.absolute()))
                 self.s3.upload_s3_obj("{}/{}".format(done_s3_key_prefix, self.exp_output_path.name), str(self.exp_output_path.absolute()))
                 self.s3.upload_s3_obj("{}/{}".format(done_s3_key_prefix, self.usage_output_path.name), str(self.usage_output_path.absolute()))
                 self.s3.upload_s3_obj("{}/{}".format(done_s3_key_prefix, self.stat_file_path.name), str(self.stat_file_path.absolute()))
                 self.s3.upload_s3_obj("{}/{}".format(done_s3_key_prefix, self.tsstat_file_path.name), str(self.tsstat_file_path.absolute()))
-
-                # clean output directory by removing all files 
-                for path in self.output_dir.iterdir():
-                    path.unlink()
 
                 # delete the key in the live link we uploaded to signify this experiment is running
                 self.s3.delete_s3_obj("{}/{}".format(live_s3_key_prefix, self.config_file_path.name))
