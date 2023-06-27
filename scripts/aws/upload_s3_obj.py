968875705ab3ce15f027a959a6905431a1aebe0e
Pranav Bhandari <pranav@mjolnir.mathcs.emory.edu>
2023-06-27 00:14:55 -0400

    update aws scripts

--- scripts/aws/upload_s3_obj.py
+++ scripts/aws/upload_s3_obj.py
@@ -3,11 +3,14 @@ from cydonia.util.S3Client import S3Client
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Download an S3 object")
-    parser.add_argument("s3_access", help="S3 key")
-    parser.add_argument("s3_secret", help="S3 secret")
     parser.add_argument("s3_key", help="S3 path for object to download")
     parser.add_argument("local_path", help="Path to block trace")
     args = parser.parse_args()
 
-    s3_client = S3Client(args.s3_access, args.s3_secret)
+    # setup S3 client 
+    aws_key = os.environ['AWS_KEY']
+    aws_secret = os.environ['AWS_SECRET']
+    aws_bucket = os.environ['AWS_BUCKET']
+
+    s3_client = S3Client(aws_key, aws_secret, aws_bucket)
     s3_client.upload_s3_obj(args.s3_key, args.local_path)
