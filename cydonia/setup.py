1bf7d6126586169154e5bab0d2d421103580487e
Pranav Bhandari <pranav@mjolnir.mathcs.emory.edu>
2023-07-23 08:11:12 -0400

    update energy measuring packge

--- cydonia/setup.py
+++ cydonia/setup.py
@@ -4,5 +4,5 @@ setup (
     name="cydonia",
     version="0.1",
     packages=['cydonia.sample', 'cydonia.cachelib', 'cydonia.util', 'cydonia.plot', 'cydonia.profiler', 'cydonia.aws'],
-    install_requires=["numpy", "pandas", "asserts", "argparse", "mmh3", "boto3", "psutil", "pyRAML"]
+    install_requires=["numpy", "pandas", "asserts", "argparse", "mmh3", "boto3", "psutil", "pyJoules"]
 )
