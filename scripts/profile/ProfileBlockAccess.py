8c7de608ef9d57b76aecfe4576c4991c56f4a355
Pranav Bhandari <pranav@mjolnir.mathcs.emory.edu>
2023-07-26 09:15:55 -0400

    profile block access

--- scripts/profile/ProfileBlockAccess.py
+++ scripts/profile/ProfileBlockAccess.py
@@ -2,7 +2,6 @@
 import sys 
 from cydonia.profiler.BlockAccessTraceProfiler import BlockAccessTraceProfiler
 
-workload = sys.argv[1]
-profiler = BlockAccessTraceProfiler("/research2/mtc/cp_traces/pranav/block_access_traces/{}.csv".format(workload))
-profiler.profile(output_path="/research2/mtc/cp_traces/pranav/block_access_window_stat/{}.csv".format(workload))
+profiler = BlockAccessTraceProfiler(sys.argv[1])
+profiler.profile(output_path=sys.argv[2])
 
