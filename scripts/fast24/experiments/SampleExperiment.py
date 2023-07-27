92c802968dcd2b9c06a377be3828af172030b258
Pranav Bhandari <pranav@mjolnir.mathcs.emory.edu>
2023-07-27 00:05:35 -0400

    merge

--- scripts/fast24/experiments/SampleExperiment.py
+++ scripts/fast24/experiments/SampleExperiment.py
@@ -39,7 +39,7 @@ class SampleExperiment:
         # Replay an ST and MT configuration with the full trace first 
         base_config = {
             "t1_size_mb": cache_size_mb,
-            "trace_s3_key": "blocktrace/{}/{}.csv".format(workload_type, workload),
+            "trace_s3_key": "blocktraces/{}/{}.csv".format(workload_type, workload),
             "kwargs": {
                 "replayRate": replay_rate
             }
@@ -60,7 +60,7 @@ class SampleExperiment:
             
             sample_st_config = deepcopy(base_config)
             sample_st_config["t1_size_mb"] = scaled_cache_size_mb
-            sample_st_config["trace_s3_key"] = "blocktrace/sample/{}/{}/{}/{}_{}_{}.csv".format(workload_type, sample_type, workload, rate, bits, self.seed)
+            sample_st_config["trace_s3_key"] = "blocktraces/sample/{}/{}/{}/{}_{}_{}.csv".format(workload_type, sample_type, workload, rate, bits, self.seed)
             experiment_list.append(sample_st_config)
 
             scaled_nvm_size_mb = int(rate * (max_cache_size_mb - cache_size_mb))
