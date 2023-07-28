6e65959cee68972bfdf3c63eea38c176693be4c2
vishwa <vishwa@localhost>
2023-07-27 23:25:47 -0500

    update Runner to track power consumption

--- cydonia/cydonia/cachelib/Runner.py
+++ cydonia/cydonia/cachelib/Runner.py
@@ -1,100 +1,123 @@
-""" This class runs a CacheBench experiment with a given configuration 
-    file. The output of the experiment and the resource (memory,CPU)
-    usage of the experiment are logged to files specified by the user. 
+"""Runner manages a process spawned using subprocess.Popen and periodically measures attributes
+    of the machine during the lifetime of the process. The following is the description of the 
+    example files created per process and its contents:
+    - /dev/shm/cydonia-runner/usage.csv - Periodic CPU and memory usage of the machine during process lifetime. 
+    - /dev/shm/cydonia-runner/power.csv - Power consumption of each CPU and memory device during process lifetime. 
+    - /dev/shm/cydonia-runner/stdout.dump - Dump of stdout. 
+    - /dev/shm/cydonia-runner/stderr.dump - Dump of stderr. 
 
-    Attributes
-    ----------
-    snapshot_window_seconds : int (Optional)(Default: 300)
-        user specified window size in seconds to snap memory and CPU usage 
+    Attributes:
+        cpu_memory_measurement_window_sec: Time period between snapshot of CPU and memory usage. 
+        terminate_event: Event that triggers termination of thread that track CPU and memory usage. 
 """
 
-import pathlib 
-import subprocess
-import psutil 
-import time 
-import sys 
-import pandas as pd 
+from time import sleep 
+from threading import Event
+from subprocess import Popen 
+from threading import Thread
+from datetime import datetime 
+from typing import List
+from psutil import virtual_memory, cpu_percent
+from pandas import read_csv, concat, DataFrame
+from pyJoules.energy_meter import EnergyContext
+from pyJoules.handler.csv_handler import CSVHandler
+
+
+def track_memory_cpu_usage_thread_function(
+    terminate_thread_event: Event, 
+    measurement_window_sec: int, 
+    output_path: str
+) -> None:
+    """Thread taking periodic snapshot of CPU and memory usage during the lifetime of the process. 
+
+    Args:
+        terminate_thread_event: Event object that is triggered to terminate the thread once the process has completed. 
+        measurement_window_sec: Time period between snapshots of CPU and memory usage. 
+        output_path: Path to file where snapshots of CPU and memory usage are stored. 
+    """
+    while True:
+        sleep(measurement_window_sec)
+        mem_stats = virtual_memory()
+        cpu_stats = cpu_percent(percpu=True)
+
+        out_dict = {}
+        out_dict["ts"] = datetime.timestamp(datetime.now())
+        out_dict["set"] = terminate_thread_event.is_set()
+        for mem_stat_field in mem_stats._fields:
+            mem_stat_value = getattr(mem_stats, mem_stat_field)
+            out_dict["mem_{}".format(mem_stat_field)] = mem_stat_value
+        for cpu_index, cpu_stat in enumerate(cpu_stats):
+            out_dict["cpu_{}".format(cpu_index)] = cpu_stat
+
+        df = DataFrame([out_dict])
+        if output_path.exists() and output_path.stat().st_size > 0:
+            old_df = read_csv(output_path)
+            df = concat([old_df, df], ignore_index=True)
+        df.to_csv(output_path, index=False)
+
+        if terminate_thread_event.is_set():
+            break 
 
 
 class Runner:
-    def __init__(self, snapshot_window_seconds=300):
+    def __init__(
+        self, 
+        cpu_memory_measurement_window_sec: int = 5
+    ) -> None:
+        """Constructor for Runner class. 
+
+            Args:
+                cpu_memory_measurement_window_sec: Time period between snapshots of CPU and memory usage. 
         """
-            Parameters
-            ----------
-            snapshot_window_seconds : int 
-                user specified window size in seconds to snap memory and CPU usage 
+        self.cpu_memory_measurement_window_sec = cpu_memory_measurement_window_sec
+
+
+    def run(
+        self, 
+        process_cmd: List[str], 
+        stdout_path: str, 
+        stderr_path: str, 
+        cpu_memory_usage_path: str, 
+        power_consumption_path: str
+    ) -> int:
+        """Spawn a process with a given command and its cpu, memory and power statistics. 
+
+            Args:
+                process_cmd: List of strings of the command to spawn a process. To spawn a process
+                    using command "ls -lh", you would pass the equivalent of "ls -lh".split(" ") which 
+                    is ["ls", "-lh"].
+                stdout_path: Path to file that stores stdout.
+                stderr_path: Path to file that stores stderr. 
+                cpu_memory_usage_path: Path to file that stores periodic CPU and memory usage during process lifetime. 
+                power_consumption_path Path to file that stores power consumption during process lifetime. 
+
+            Returns:
+                exit_code: The exit code of the process. 
         """
-        self.snapshot_window_seconds = snapshot_window_seconds
+        exit_code = -1 
 
+        # Start the thread that tracks CPU and memory usage.
+        terminate_event = Event() 
+        memory_cpu_usage_thread = Thread(
+            target=track_memory_cpu_usage_thread_function, 
+            args=(terminate_event, self.cpu_memory_measurement_window_sec, cpu_memory_usage_path))
+        memory_cpu_usage_thread.start()
 
-    def snap_system_stats(self, output_path):
-        """ Write memory and CPU stats to a file. 
-
-            Parameters
-            ----------
-            usage_handle : int 
-                the handle of the file where memory and CPU usage are logged 
-        """
-        mem_stats = psutil.virtual_memory()
-
-        out_dict = {}
-        for mem_stat_field in mem_stats._fields:
-            mem_stat_value = getattr(mem_stats, mem_stat_field)
-            if type(mem_stat_value) == str:
-                out_dict["mem_{}".format(mem_stat_field)] = mem_stat_value
-
-        cpu_stats = psutil.cpu_percent(percpu=True)
-        for cpu_index, cpu_stat in enumerate(cpu_stat_name):
-            out_dict["cpu_{}".format(cpu_index)] = cpu_stat
-
-        df = pd.DataFrame([out_dict])
-        if output_path.exists():
-            old_df = pd.read_csv(output_path)
-            df = pd.concat([old_df, df], ignore_index=True)
-        print(df)
-        df.to_csv(output_path, index=False)
-
-
-    def run(self, cachebench_binary_path, cachebench_config_file_path, experiment_output_path, resource_usage_output_path):
-        """ Run the CacheBench experiment and track its memory and CPU 
-            usage. 
-
-            Parameters
-            ----------
-            cachebench_binary_path : pathlib.Path
-                path to the cachebench binary 
-            cachebench_config_file_path : pathlib.Path
-                path to JSON file containing CacheBench configuration 
-            experiment_output_path : pathlib.Path 
-                path to the directory where stdout is dumped 
-            resource_usage_output_path : pathlib.Path 
-                path to the directory where the resource usage file is stored 
-            
-            Return 
-            ------
-            return_code : int 
-                the status of experiment process on termination 
-        """
-        return_code = -1 
-        start_time = time.time()
-        with experiment_output_path.open("w+") as output_handle, \
-                resource_usage_output_path.open("w+") as usage_handle:
-
-            # run the cachelib experiment 
-            with subprocess.Popen([cachebench_binary_path, 
-                                    "--json_test_config", 
-                                    str(cachebench_config_file_path)], 
-                                    stdout=subprocess.PIPE, 
-                                    stderr=subprocess.STDOUT, 
-                                    bufsize=1,
-                                    encoding='utf-8') as process_handle:
-
-                while process_handle.poll() is None:
-                    time.sleep(self.snapshot_window_seconds)
-                    self.snap_system_stats(resource_usage_output_path)
-                            
-                # wait for it to complete 
-                process_handle.wait()
-                return_code = process_handle.returncode
+        # Start the process within the an energy context that tracks power consumption. 
+        power_csv_handler = CSVHandler(power_consumption_path)
+        with EnergyContext(handler=power_csv_handler) as energy_ctx, \
+            open(stdout_path, "w+") as stdout_handle, \
+                open(stderr_path, "w+") as stderr_handle:
         
-        return return_code
+            with Popen(process_cmd, 
+                stdout=stdout_handle, 
+                stderr=stderr_handle
+            ) as process_handle:
+                process_handle.wait()
+                exit_code = process_handle.returncode
+        
+        # Notify the thread tracking CPU and memory usage to terminate and wait. 
+        terminate_event.set()
+        memory_cpu_usage_thread.join()
+        power_csv_handler.save_data()
+        return exit_code
--- scripts/fast24/RunExperiment.py
+++ scripts/fast24/RunExperiment.py
@@ -13,41 +13,69 @@ from pyJoules.energy_meter import measure_energy
 from pyJoules.handler.csv_handler import CSVHandler
 
 
+STDOUT_FILENAME = "stdout.dump"
+STDERR_FILENAME = "stderr.dump"
+CONFIG_FILENAME = "config.json"
+POWER_FILENAME = "power.csv"
+CPU_MEM_USAGE_FILENAME = "usage.csv"
+STAT_FILENAME = "stat_0.out"
+TS_STAT_FILENAME = "tsstat_0.out"
+
 BACKING_FILE_PATH = pathlib.Path.home().joinpath("disk/disk.file")
 NVM_FILE_PATH = pathlib.Path.home().joinpath("nvm/disk.file")
 EXPERIMENT_FILE_PATH = pathlib.Path("experiments.json")
 CACHEBENCH_BINARY_PATH = pathlib.Path.home().joinpath("disk/CacheLib/opt/cachelib/bin/cachebench")
-OUTPUT_DIR = pathlib.Path("/dev/shm")
+OUTPUT_DIR = pathlib.Path("/dev/shm/")
+OUTPUT_DIR.mkdir(exist_ok=True, parents=True)
 POWER_FILE_PATH = OUTPUT_DIR.joinpath("power.csv")
 
 csv_handler = CSVHandler(str(POWER_FILE_PATH.absolute()))
 
 
 class RunExperiment:
-    def __init__(self, machine_name, experiment_file_path, backing_file_path, nvm_file_path, cachebench_binary_path, output_dir, num_itr):
+    def __init__(self, 
+        machine_name: str, 
+        experiment_file_path: str, 
+        backing_file_path: str, 
+        nvm_file_path: str, 
+        cachebench_binary_path: str, 
+        output_dir: str, 
+        num_itr: int):
+        """Constructor where we setup necessary files before running block trace replay. 
+
+            Args:
+                machine_name: Name of the machine where trace replay is running. 
+                experiment_file_path: Path to file containing a list of block trace replay to run. 
+                backing_file_path: Path to a file in backing storage. 
+                nvm_file_path: Path to a file in NVM storage. 
+                cachebench_binary_path: Path to the cachebench binary. 
+                output_dir: Path to directory where we store output files from block trace replay. 
+                num_itr: Number of iteration to run an experiment. 
+        """
+        self.machine_name = machine_name
         self.experiment_file_path = experiment_file_path
         with open(experiment_file_path) as f:
             self.experiment_list = json.load(f)
-
         self.backing_file_path = backing_file_path
         self.nvm_file_path = nvm_file_path
         self.cachebench_binary_path = cachebench_binary_path
         self.output_dir = output_dir
         self.output_dir.mkdir(exist_ok=True)
-        self.config_file_path = self.output_dir.joinpath("config.json")
-        self.exp_output_path = self.output_dir.joinpath("stdout.dump")
-        self.power_usage_path = POWER_FILE_PATH
-        self.usage_output_path = self.output_dir.joinpath("usage.csv")
-        self.stat_file_path = self.output_dir.joinpath("stat_0.out")
-        self.tsstat_file_path = self.output_dir.joinpath("tsstat_0.out")
-        self.hostname = socket.gethostname()
-        self.machine_name = machine_name
         self.num_itr = num_itr
+
+        self.stdout_path = self.output_dir.joinpath(STDOUT_FILENAME)
+        self.stderr_path = self.output_dir.joinpath(STDERR_FILENAME)
+        self.config_file_path = self.output_dir.joinpath(CONFIG_FILENAME)
+        self.power_consumption_path = self.output_dir.joinpath(POWER_FILENAME)
+        self.usage_output_path = self.output_dir.joinpath(CPU_MEM_USAGE_FILENAME)
+        self.stat_file_path = self.output_dir.joinpath(STAT_FILENAME)
+        self.tsstat_file_path = self.output_dir.joinpath(TS_STAT_FILENAME)
 
         self.aws_key = os.environ['AWS_KEY']
         self.aws_secret = os.environ['AWS_SECRET']
         self.aws_bucket = os.environ['AWS_BUCKET']
         self.s3 = S3Client(self.aws_key, self.aws_secret, self.aws_bucket)
+        self.hostname = socket.gethostname()
 
 
     def experiment_running(self, config, workload, cur_iteration):
@@ -77,15 +105,64 @@ class RunExperiment:
                                                                                     replay_rate,
                                                                                     cur_iteration)
     
-    @measure_energy(handler=csv_handler)
+
     def _run(self):
-        
         runner = Runner()
-        return_code = runner.run(self.cachebench_binary_path, 
-                                    self.config_file_path, 
-                                    self.exp_output_path, 
-                                    self.usage_output_path)
+        return_code = runner.run([self.cachebench_binary_path, "--json_test_config", self.config_file_path], 
+                                    self.stdout_path, 
+                                    self.stderr_path,
+                                    self.usage_output_path,
+                                    self.power_consumption_path)
+        
         return return_code
+
+
+    def clean(self):
+        self.stdout_path.unlink()
+        self.stderr_path.unlink()
+        self.usage_output_path.unlink()
+        self.power_consumption_path.unlink()
+        self.stat_file_path.unlink()
+        self.tsstat_file_path.unlink()
+
+    
+    def upload_experiment_files(
+        self, 
+        s3_key_prefix: str 
+    ) -> None:
+        """Upload experiment output files to S3. 
+
+        Args:
+            s3_key_prefix: The prefix to add behind the file name to construct the S3 key. 
+        """
+        self.s3.upload_s3_obj(
+            "{}/{}".format(s3_key_prefix, self.config_file_path.name), 
+            str(self.config_file_path.absolute()))
+
+        self.s3.upload_s3_obj(
+            "{}/{}".format(s3_key_prefix, self.stdout_path.name), 
+            str(self.stdout_path.absolute()))
+
+        self.s3.upload_s3_obj(
+            "{}/{}".format(s3_key_prefix, self.stderr_path.name), 
+            str(self.stderr_path.absolute()))
+
+        self.s3.upload_s3_obj(
+            "{}/{}".format(s3_key_prefix, self.power_consumption_path.name), 
+            str(self.power_consumption_path.absolute()))
+
+        self.s3.upload_s3_obj(
+            "{}/{}".format(s3_key_prefix, self.usage_output_path.name), 
+            str(self.usage_output_path.absolute()))
+
+        self.s3.upload_s3_obj(
+            "{}/{}".format(s3_key_prefix, self.stat_file_path.name), 
+            str(self.stat_file_path.absolute()))
+
+        self.s3.upload_s3_obj(
+            "{}/{}".format(s3_key_prefix, self.tsstat_file_path.name), 
+            str(self.tsstat_file_path.absolute()))
+
 
 
     def run(self):
@@ -123,29 +200,19 @@ class RunExperiment:
                 self.s3.download_s3_obj(experiment_entry["trace_s3_key"], str(local_trace_path.absolute()))
 
                 return_code = self._run()
-                csv_handler.save_data()
                 print("Completed-> Experiment {},{} with return code {}", config.get_config(), cur_iteration, return_code)
                 if return_code == 0:
                     done_s3_key_prefix = self.get_s3_key("done", workload_key_str, config.get_config(), cur_iteration)
-                    self.s3.upload_s3_obj("{}/{}".format(done_s3_key_prefix, self.config_file_path.name), str(self.config_file_path.absolute()))
-                    self.s3.upload_s3_obj("{}/{}".format(done_s3_key_prefix, self.exp_output_path.name), str(self.exp_output_path.absolute()))
-                    self.s3.upload_s3_obj("{}/{}".format(done_s3_key_prefix, self.usage_output_path.name), str(self.usage_output_path.absolute()))
-                    self.s3.upload_s3_obj("{}/{}".format(done_s3_key_prefix, self.stat_file_path.name), str(self.stat_file_path.absolute()))
-                    self.s3.upload_s3_obj("{}/{}".format(done_s3_key_prefix, self.tsstat_file_path.name), str(self.tsstat_file_path.absolute()))
-                    self.s3.upload_s3_obj("{}/{}".format(done_s3_key_prefix, self.power_usage_path.name), str(self.power_usage_path.absolute()))
-                    # delete the key in the live link we uploaded to signify this experiment is running
+                    self.upload_experiment_files(done_s3_key_prefix)
                     self.s3.delete_s3_obj("{}/{}".format(live_s3_key_prefix, self.config_file_path.name))
                     print("Done-> Experiment {},{}", config.get_config(), cur_iteration)
+                    self.clean()
                 else:
                     error_s3_key_prefix = self.get_s3_key("error", workload_key_str, config.get_config(), cur_iteration)
-                    self.s3.upload_s3_obj("{}/{}".format(error_s3_key_prefix, self.config_file_path.name), str(self.config_file_path.absolute()))
-                    self.s3.upload_s3_obj("{}/{}".format(error_s3_key_prefix, self.exp_output_path.name), str(self.exp_output_path.absolute()))
-                    self.s3.upload_s3_obj("{}/{}".format(error_s3_key_prefix, self.usage_output_path.name), str(self.usage_output_path.absolute()))
-                    self.s3.upload_s3_obj("{}/{}".format(error_s3_key_prefix, self.stat_file_path.name), str(self.stat_file_path.absolute()))
-                    self.s3.upload_s3_obj("{}/{}".format(error_s3_key_prefix, self.tsstat_file_path.name), str(self.tsstat_file_path.absolute()))
+                    self.upload_experiment_files(error_s3_key_prefix)
                     print("Error-> Experiment {},{}", config.get_config(), cur_iteration)
-                    # delete the key in the live link we uploaded to signify this experiment is running
                     self.s3.delete_s3_obj("{}/{}".format(live_s3_key_prefix, self.config_file_path.name))
+                    self.clean()
                     return 
 
 
